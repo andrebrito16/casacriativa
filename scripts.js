fd0a591d35bb2c86feea2e0e60a80414c701ab18
andrebrito16 <felipe.brito016@gmail.com>
2020-03-28 22:31:42 -0400

    pageideias

--- scripts.js
+++ scripts.js
@@ -3,4 +3,12 @@ function onShow(){
         .querySelector("#modal")
         .classList
         .toggle('hide')
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")   
+    document    
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
 }
